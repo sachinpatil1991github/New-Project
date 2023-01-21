c9132b177e63da8f6d4147f5577ff52007c5d735
sachin <sachinpatil.mech91@gmail.com>
2023-01-21 10:30:01 -0500

    Added array list

---
+++ src/com/sachin/practice/Demo.java
@@ -0,0 +1,19 @@
+package com.sachin.practice;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+
+		ArrayList<String> list = new ArrayList<String>();
+
+		list.add("java");
+		list.add("Hibernate");
+		list.add("spring");
+		
+		System.out.println(list);
+	}
+
+}
